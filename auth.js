d09068014e586c7b27f61f5279a33a062887ca4f
shabna8769 <64775817+shabna8769@users.noreply.github.com>
2021-08-08 15:45:05 +0530

    Delete auth.js

--- auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-//@route GET api/Auth
-//@desc Test route
-//@access Public
-
-router.get('/', (req, res) => res.send('Auth route'));
-module.export = router;
